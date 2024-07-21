8e5fe5544f0faa6daa63f3fb852db92b8a1b96c5
lukagugushvili <gugushvili58@gmail.com>
2024-07-21 07:57:25 -0700

    Add getAll and getId methods to UserService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { data } from './data/data';
 
 @Injectable()
 export class UsersService {
+  private users = data;
+
   getUsers() {
-    return 'users hi';
+    if (this.users.length === 0) {
+      throw new HttpException('No users available', HttpStatus.NOT_FOUND);
+    }
+
+    return this.users;
+  }
+
+  getUsersById(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new HttpException(
+        `User with id: ${id} does not exist`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return user;
   }
 }
